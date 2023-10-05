7b113c292f13e7d2ea1d85b6845dedeef71f46a7
milagros <miliguzman0@gmail.com>
2023-10-05 00:58:24 -0300

    feat(server/src/models)createdModelComments

---
+++ server/src/models/comments.ts
@@ -0,0 +1,56 @@
+import { DataTypes, Model, Sequelize, UUIDV4 } from 'sequelize';
+
+interface CommentAttributes{
+  comment_id: string;
+  user_id: string;
+  tweet_id: string;
+  text: string;
+  multimedia_id:string | null;
+  createdAt: Date;
+}
+module.exports = (sequelize : Sequelize)=>{
+    class Comments extends Model<CommentAttributes> implements CommentAttributes {
+        public comment_id!: string;
+        public user_id!: string;
+        public tweet_id!: string;
+        public text!: string;
+        public multimedia_id!: string | null;
+        public createdAt!: Date;
+    }
+    Comments.init(
+        {
+            comment_id: {
+                type: DataTypes.UUID,
+                defaultValue: UUIDV4,
+                primaryKey: true,
+            },
+            user_id:{
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            tweet_id:{
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            text:{
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            multimedia_id:{
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            createdAt:{
+                type: DataTypes.DATE,
+                defaultValue: DataTypes.NOW,
+                allowNull: false,
+            }
+        },
+        {
+        sequelize,
+        modelName: 'Comments',
+        timestamps: false,
+        }
+    );
+    return Comments;
+};
