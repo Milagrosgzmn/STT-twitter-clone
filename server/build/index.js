74a0506d8224bad932f7690c973c84918e5d66e9
milagros <miliguzman0@gmail.com>
2023-10-05 00:57:20 -0300

    added(server/src)addedOtherConfigurations

---
+++ server/build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const express = require('express');
+const server = express();
+const PORT = process.env.PORT || 3000;
+server.use(express.json());
+server.listen(PORT, () => {
+    console.log(`Servidor levantado con exito en el puerto : ${PORT}`);
+});
