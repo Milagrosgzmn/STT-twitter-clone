bff0176f75f45595970834390f184c12eb183805
milagros <miliguzman0@gmail.com>
2023-10-19 21:20:19 -0300

    feat(server):addedSignUpController

--- server/src/controllers/users/postUser.ts
+++ server/src/controllers/users/postUser.ts
@@ -1,12 +1,23 @@
 import { User } from "../../types/types";
+const bcrypt = require('bcrypt');
 const {Users} = require('../../DB_connection');
 const postUser = async(user: User)=>{
     try {
+        const hashedPassword = await bcrypt.hash(user.user_password, 10);
+        user.user_password = hashedPassword;
         const created = await Users.create(user);
-
-        return 'good';
-    } catch (error) {
-        return 'error';
+        if (created) {
+            return 'success'
+        }
+    } catch (error:any) {
+        if (error.name === 'SequelizeUniqueConstraintError') {
+            console.error(error);
+            return `Duplicated key ${error.message}`
+            
+          } else {
+            console.error('Error while creating new User:', error);
+            return error.message;
+          }
     }
 }
 
